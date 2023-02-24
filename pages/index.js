129438b63d4a026071b493ca72d6b6c2958714bb
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-02-23 23:56:38 -0600

    Haciendo SASS global con next config

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome</h1>
-      <button>Click</button>
+      <h1>Hello form H1</h1>
     </div>
   )
 }
