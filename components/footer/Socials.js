6aed957fc820b895040b6aee7ad7bb7b4448ff11
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-03-05 20:11:22 -0600

    Agregamos en el footer redes sociales y newsletter

---
+++ components/footer/Socials.js
@@ -0,0 +1,51 @@
+import styles from './styles.module.scss';
+import Link from 'next/link';
+import {FaFacebookF, FaTiktok} from 'react-icons/fa';
+import {BsInstagram, BsTwitter, BsYoutube, BsPinterest, BsSnapchat} from 'react-icons/bs';
+
+export default function Socials() {
+  return (
+    <div className={styles.footer__socials}>
+        <section>
+            <h3>STAY CONNECTED</h3>
+            <ul>
+                <li>
+                    <a href='' target='_blank' rel='noopener noreferrer'>
+                        <FaFacebookF/> 
+                    </a>
+                </li>
+                <li>
+                    <a href='' target='_blank' rel='noopener noreferrer'>
+                        <BsInstagram/> 
+                    </a>
+                </li>
+                <li>
+                    <a href='' target='_blank' rel='noopener noreferrer'>
+                        <FaTiktok/> 
+                    </a>
+                </li>
+                <li>
+                    <a href='' target='_blank' rel='noopener noreferrer'>
+                        <BsTwitter/> 
+                    </a>
+                </li>
+                <li>
+                    <a href='' target='_blank' rel='noopener noreferrer'>
+                        <BsYoutube/> 
+                    </a>
+                </li>
+                <li>
+                    <a href='' target='_blank' rel='noopener noreferrer'>
+                        <BsPinterest/> 
+                    </a>
+                </li>
+                <li>
+                    <a href='' target='_blank' rel='noopener noreferrer'>
+                        <BsSnapchat/> 
+                    </a>
+                </li>
+            </ul>
+        </section>
+    </div>
+  )
+}
