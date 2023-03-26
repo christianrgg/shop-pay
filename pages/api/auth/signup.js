21bb3cf38eb5620a1b4d16b8c841b5c25faf7a40
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-03-25 19:25:53 -0600

    Agregamos validaciones desde el servidor,  encriptación de contraseñas, y token de usuario

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,9 @@
 import nextConnect from "next-connect";
+import bcrypt from "bcrypt"
 import { validateEmail } from "../../../utils/validation";
 import db from "../../../utils/db"
+import User from "../../../models/User";
+import { createActivationToken } from "../../../utils/tokens";
 
 const handler = nextConnect();
 
@@ -15,8 +18,31 @@ handler.post(async(req,res)=>{
         if(!validateEmail(email)){
             return res.status(400).json({message:"Invalid email."})
         }
+        const user = await User.findOne({email});
+        if(user){
+            return res
+            .status(400)
+            .json({message:"This email already exists."});
+        }
+        if(password.length<6){
+            return res
+            .status(400)
+            .json({message:"Password must be atleast 6 characters."});
+        }
+        const cryptedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({name,email,password:cryptedPassword});
+        const addedUser = await newUser.save();
+        const activation_token = createActivationToken({
+            id: addedUser._id.toString(),
+
+        });
+        // res.send(addedUser);
+        console.log(activation_token);
+        res.send(activation_token)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        // res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message, errorLine: error.stack})
+        
     }
 
 });
