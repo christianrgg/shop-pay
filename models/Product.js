b6fdedebdf9319a5298aaf5e8e0175566be98b2c
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-04-20 12:52:31 -0600

    Modelo de producto

---
+++ models/Product.js
@@ -0,0 +1,129 @@
+import mongoose from "mongoose";
+
+const {ObjectId} = mongoose.Schema;
+const reviewSchema = new mongoose.Schema({
+    reviewBy: {
+        type: ObjectId,
+        ref: "User",
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        deafult: 0,
+    },
+    review: {
+        type: String,
+        required: true,
+    },
+    size: {
+        type: String,
+    },
+    style: {
+        color: String,
+        image: String,
+    },
+    fit: {
+        type: String,
+    },
+    images:[],
+    likes:[],
+})
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true,
+    },
+    description: {
+        type: String,
+        required:true,
+    },
+    brand: {
+        type: String,
+    },
+    slug: {
+        type: String,
+        required:true,
+    },
+    category: {
+        type: String,
+        required:true,
+        ref: "Category",
+    },
+    subcategories: [
+        {
+            type: ObjectId,
+            ref: "subcategory",
+        }
+    ],
+    details:[
+        {
+            name: String,
+            value: String,
+            
+        }
+    ],
+    question:[
+        {
+            question: String,
+            answer: String,
+        }
+    ],
+    reviews:[reviewSchema],
+    refundPolicy:{
+        type: String,
+        default: "30 days",
+    },
+    rating:{
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    numberReviews:{
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    shipping:{
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    subProducts:[
+        {
+            images:[],
+            description_images:[],
+            color: {
+                color: {
+                    type: String,
+                },
+                image: {
+                    type: String,
+                },
+            },
+            sizes:[
+                {
+                    size: String,
+                    qty: Number,
+                    price: Number,
+                }
+            ],
+            discount: {
+                type: Number,
+                default: 0,
+            },
+            sold: {
+                type: Number,
+                default: 0,
+            },
+        },
+    ],
+},
+{
+    timestamps: true,
+}
+);
+
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
