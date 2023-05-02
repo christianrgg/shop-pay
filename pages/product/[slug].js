0a2ca03d36103835198295e63a7ad91585e7c9f3
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-01 21:17:12 -0600

    Información de producto: Rating, rango de precio, porcentaje de descuento

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,6 +8,7 @@ import Header from "../../components/header"
 import Footer from "../../components/footer"
 import MainSwiper from "../../components/productPage/mainSwiper"
 import { useState } from "react"
+import Infos from "../../components/productPage/infos"
 
 export default function product({product}) { 
     const [activeImg, setActiveImg] = useState("")
@@ -28,7 +29,7 @@ export default function product({product}) {
                 </div>
                 <div className={styles.product__main}>
                     <MainSwiper images={product.images} activeImg={activeImg}/>
-                    <div></div>
+                    <Infos product={product} setActiveImg={setActiveImg}/>
                 </div>
             </div>
         </div>
