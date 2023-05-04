3062e9b4c8e73ad82f61a08d804414c9c0ad9c7e
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-03 22:39:11 -0600

    Correcion de Rutas deploy

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import db from "@/utils/db"
-export default function handler(req, res) {
-  db.connectDB();
-  db.disconnectDb();
-  res.status(200).json({ name: 'John Doe' })
-}
+// import db from "@/utils/db"
+// export default function handler(req, res) {
+//   db.connectDB();
+//   db.disconnectDb();
+//   res.status(200).json({ name: 'John Doe' })
+// }
