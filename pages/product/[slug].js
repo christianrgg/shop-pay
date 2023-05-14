70ef6cda71366ee73dcc74f3a3a3f99754d6fa94
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-13 19:21:49 -0600

    Pagina carrito: carrito vacio

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -36,6 +36,7 @@ export default function ProductPage({product}) {
                 </div>
                 <Reviews product={product}/>
             </div>
+        <Footer country=""/>
         </div>
     </>
   )
