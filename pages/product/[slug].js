5e2324a0b653a7daf7747db77c62d3f35e779ec8
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-13 22:26:22 -0600

    Pagina Carrito: obtener detalles del producto

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -66,6 +66,7 @@ export async function getServerSideProps(context) {
 
     let newProduct = {
         ...product,
+        style,
         images: subProduct.images,
         sizes: subProduct.sizes,
         discount: subProduct.discount,
