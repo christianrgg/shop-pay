4a1509817c61f92cdb1fcd1f99f22c8b0b32308f
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-16 03:25:29 -0600

    Pagina Carrito: renderizando productos en carrito

--- pages/cart.js
+++ pages/cart.js
@@ -1,15 +1,25 @@
+import { useSelector } from "react-redux";
 import Empty from "../components/cart/empty";
 import Header from "../components/cart/header/index";
 import styles from "../styles/cart.module.scss"
+import Product from "../components/cart/product";
 
-export default function cart() {
-  const cart = [];
+export default function Cart() {
+  const {cart} = useSelector((state) => ({...state}));
     return (
     <>
         <Header/>
         <div className={styles.cart}>
-            {cart.length > 1 ? (
-            <div className={styles.cart__container}></div> 
+            {cart.cartItems.length >= 1 ? (
+            <div className={styles.cart__container}>
+              <div className={styles.cart__products}>
+                {
+                  cart.cartItems.map((product)=>(
+                    <Product product={product} key={product._uid}/> 
+                  ))
+                }
+              </div>
+            </div> 
             ): ( 
                 <Empty/>
                 )}
