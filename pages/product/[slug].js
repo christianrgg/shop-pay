653fa7c3f0b5cd5411e5ceb743dad2e197a86eea
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-28 00:03:43 -0600

    Ajuste de errores: imagenes dinamicas, subproducto 0

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -50,169 +50,82 @@ export default function ProductPage({product}) {
   )
 }
 
-// export async function getServerSideProps(context) {
-//     const {query} = context;
-//     const slug    = query.slug;
-//     const style   = query.style || 0;
-//     const size    = query.size || 0;
-//     // console.log(slug, style, size);
-//     db.connectDB();
-//     //-------------
-//     let product = await Product.findOne({slug})
-//         .populate({path:"category", model:Category})
-//         .populate({path:"subCategories._id", model:SubCategory, strictPopulate: false})
-//         .populate({path:"reviews.reviewBy", model:User, strictPopulate: false})
-//         .lean();
-//     let subProduct = product?.subProducts?.[style];
-//     let prices = subProduct?.sizes?.map((s) => s.price)?.sort((a, b) => a - b) || [];
-//     // let subProduct = product?.subProducts?.[style];
-//     // let prices = (subProduct?.sizes?.map((s)=>{
-//     //     return s.price;
-//     // }))
-//     // .sort((a,b)=>{
-//     //     return a - b;
-//     // });
-
-//     let newProduct = {
-//         ...product,
-//         style:parseInt(style),
-//         images: subProduct?.images,
-//         sizes: subProduct?.sizes,
-//         discount: subProduct?.discount,
-//         sku: subProduct?.sku,
-//         colors: product?.subProducts?.map((p)=>{
-//             return p.color;
-//         }),
-//         priceRange: subProduct?.discount 
-//             ?`From ${(prices[0]-prices[0] / subProduct.discount).toFixed(2)} to ${
-//                 (prices[prices.length-1] - 
-//                 prices[prices.length-1] / subProduct.discount).toFixed(2)
-//             }$` 
-//             : `From ${prices[0]} to ${prices[prices.length-1]}$`,
-//         price: subProduct?.discount> 0 
-//         ? (subProduct?.sizes?.[size]?.price -
-//           (subProduct?.sizes?.[size]?.price / subProduct.discount)).toFixed(2)
-//         : subProduct?.sizes?.[size]?.price,
-//         priceBefore: subProduct?.sizes?.[size]?.price,
-//         quantity: subProduct?.sizes?.[size]?.qty,
-//         ratings:[
-//             {
-//                 "percentage":76,
-//             },
-//             {
-//                 "percentage":14,
-//             },
-//             {
-//                 "percentage":6,
-//             },
-//             {
-//                 "percentage":4,
-//             },
-//             {
-//                 "percentage":0,
-//             }
-//         ],
-//         allSizes: (product.subProducts.map((p)=>{
-//             return p.sizes;
-//         })).flat().sort((a,b)=>{
-//             return a.size - b.size
-//         }).filter((element,index,array)=>array.findIndex((el2)=>el2.size===element.size)=== index),
-//     };
-//     //-------------
-//     db.disconnectDb();
-//     return {
-//         props: {
-//             product: JSON.parse(JSON.stringify(newProduct))
-//         },
-//     }
-// }
 export async function getServerSideProps(context) {
-    const { query } = context;
-    const slug = query.slug;
-    const style = parseInt(query.style) || 0;
-    const size = query.size || 0;
-  
-    try {
-      await db.connectDB(); // Conecta a la base de datos
-  
-      let product = await Product.findOne({ slug })
+    const {query} = context;
+    const slug    = query.slug;
+    const style   = query.style || 0;
+    const size    = query.size || 0;
+    db.connectDB();
+    //-------------
+    let product = await Product.findOne({slug})
+        // .populate({path:"category", model:Category})
+        // .populate({path:"subCategories._id", model:SubCategory, strictPopulate: false})
         .populate({ path: "category", model: Category })
         .populate({ path: "subCategories", model: SubCategory, strictPopulate: false })
-        // .populate({ path: "subCategories._id", model: SubCategory, strictPopulate: false })
-        .populate({ path: "reviews.reviewBy", model: User, strictPopulate: false })
+        .populate({path:"reviews.reviewBy", model:User, strictPopulate: false})
         .lean();
-  
-    let activeStyle = Math.min(parseInt(style) || 0, (product.images && product.images.length > 0) ? product.images.length - 1 : 0);
-      let subProduct = product?.subProducts?.[activeStyle];
-      let prices = subProduct?.sizes?.map((s) => s.price)?.sort((a, b) => a - b) || [];
-  
-      let newProduct = {
+    // let subProduct = product?.subProducts?.[style];
+    let subProduct = product?.subProducts?.[style] || product?.subProducts?.[0];
+    let prices = subProduct?.sizes?.map((s) => s.price)?.sort((a, b) => a - b) || [];
+    // let subProduct = product?.subProducts?.[style];
+    // let prices = (subProduct?.sizes?.map((s)=>{
+    //     return s.price;
+    // }))
+    // .sort((a,b)=>{
+    //     return a - b;
+    // });
+
+    let newProduct = {
         ...product,
-        style: activeStyle,
+        style,
         images: subProduct?.images,
         sizes: subProduct?.sizes,
         discount: subProduct?.discount,
         sku: subProduct?.sku,
-        colors: product?.subProducts?.map((p) => {
-          return p.color;
+        colors: product?.subProducts?.map((p)=>{
+            return p.color;
         }),
-        priceRange: subProduct?.discount
-          ? `From ${(prices[0] - prices[0] / subProduct.discount).toFixed(2)} to ${
-              (prices[prices.length - 1] - prices[prices.length - 1] / subProduct.discount).toFixed(2)
-            }$`
-          : `From ${prices[0]} to ${prices[prices.length - 1]}$`,
-        price:
-          subProduct?.discount > 0
-            ? (subProduct?.sizes?.[size]?.price - (subProduct?.sizes?.[size]?.price / subProduct.discount)).toFixed(2)
-            : subProduct?.sizes?.[size]?.price,
+        priceRange: subProduct?.discount 
+            ?`From ${(prices[0]-prices[0] / subProduct.discount).toFixed(2)} to ${
+                (prices[prices.length-1] - 
+                prices[prices.length-1] / subProduct.discount).toFixed(2)
+            }$` 
+            : `From ${prices[0]} to ${prices[prices.length-1]}$`,
+        price: subProduct?.discount> 0 
+        ? (subProduct?.sizes?.[size]?.price -
+          (subProduct?.sizes?.[size]?.price / subProduct.discount)).toFixed(2)
+        : subProduct?.sizes?.[size]?.price,
         priceBefore: subProduct?.sizes?.[size]?.price,
         quantity: subProduct?.sizes?.[size]?.qty,
-        ratings: [
-          {
-            percentage: 76,
-          },
-          {
-            percentage: 14,
-          },
-          {
-            percentage: 6,
-          },
-          {
-            percentage: 4,
-          },
-          {
-            percentage: 0,
-          },
+        ratings:[
+            {
+                "percentage":76,
+            },
+            {
+                "percentage":14,
+            },
+            {
+                "percentage":6,
+            },
+            {
+                "percentage":4,
+            },
+            {
+                "percentage":0,
+            }
         ],
-        allSizes: product.subProducts
-          .map((p) => {
+        allSizes: (product.subProducts.map((p)=>{
             return p.sizes;
-          })
-          .flat()
-          .sort((a, b) => {
-            return a.size - b.size;
-          })
-          .filter((element, index, array) => array.findIndex((el2) => el2.size === element.size) === index),
-      };
-  
-      return {
-        props: {
-          product: JSON.parse(JSON.stringify(newProduct)),
-        },
-      };
-    } catch (error) {
-      // Manejo de errores
-    console.error("Error en getServerSideProps:", error);
-
-    // Desconexión de la base de datos (en caso de error)
-    await db.disconnectDb();
-
-    // Puedes retornar un objeto con un error personalizado en caso de que ocurra un error en la obtención de datos
-    return {
-      props: {
-        error: "Error al obtener los datos del producto",
-      },
+        })).flat().sort((a,b)=>{
+            return a.size - b.size
+        }).filter((element,index,array)=>array.findIndex((el2)=>el2.size===element.size)=== index),
     };
-  }
+    //-------------
+    db.disconnectDb();
+    return {
+        props: {
+            product: JSON.parse(JSON.stringify(newProduct))
+        },
+    }
 }
       
